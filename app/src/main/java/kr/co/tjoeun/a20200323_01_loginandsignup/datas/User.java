7e294d6c43162232da4e8799c2906ff5353a8248
potential20 <passion212@nate.com>
2020-03-23 16:36:11 +0900

    JSON -> User객체 변환 메쏘드 생성

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/datas/User.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/datas/User.java
@@ -1,5 +1,8 @@
 package kr.co.tjoeun.a20200323_01_loginandsignup.datas;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
@@ -9,6 +12,24 @@ public class User implements Serializable {
     private String name;
     private String phone;
     private String memo;
+
+//    JSONObject를 재료로 넣으면 => User객체로 돌려주는 메쏘드 static
+    public static User getUserFromJson (JSONObject object) {
+        User user = new User();
+
+        try {
+            user.id = object.getInt("id");
+            user.loginId = object.getString("login_id");
+            user.name = object.getString("name");
+            user.phone = object.getString("phone");
+            user.memo = object.getString("momo");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return user;
+    }
 
     public User() {
     }
