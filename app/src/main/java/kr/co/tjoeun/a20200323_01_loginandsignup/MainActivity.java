503898c303eab85396dc39a07e11302abe79a1d5
potential20 <passion212@nate.com>
2020-03-23 10:19:10 +0900

    로그인 버튼 누르면 => 이메일 저장  - 화면이 켜질때 => 저장된 이메일 자동 작성

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
+import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ContextUtil;
 
 public class MainActivity extends BaseActivity {
 
@@ -22,10 +24,23 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+//        로그인 버튼을 누르면 => 입력되어있는 이메일 저장
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputEmail = binding.emailEdt.getText().toString();
+
+                ContextUtil.setEmail(mContext, inputEmail);
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
+
+//        이 화면을 키면 저장된 이메일 값을 emailEdt에 입력.
+        binding.emailEdt.setText(ContextUtil.getEmail(mContext));
 
     }
 }
