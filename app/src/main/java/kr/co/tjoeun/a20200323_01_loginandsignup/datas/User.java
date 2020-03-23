191e17cd4bc4624d024113670ca8c2a736b5692b
potential20 <passion212@nate.com>
2020-03-23 16:17:39 +0900

    User정보를 담아주는 클래스 생성

---
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/datas/User.java
@@ -0,0 +1,63 @@
+package kr.co.tjoeun.a20200323_01_loginandsignup.datas;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private int id;
+    private String loginId;
+    private String name;
+    private String phone;
+    private String memo;
+
+    public User() {
+    }
+
+    public User(int id, String loginId, String name, String phone, String memo) {
+        this.id = id;
+        this.loginId = loginId;
+        this.name = name;
+        this.phone = phone;
+        this.memo = memo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+}
