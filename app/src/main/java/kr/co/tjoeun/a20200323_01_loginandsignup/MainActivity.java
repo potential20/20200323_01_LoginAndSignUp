3393f6a47d84db2a736a79dc1b0a6ec23becbc8a
potential20 <passion212@nate.com>
2020-03-23 18:26:29 +0900

    사용자 정보 실제로 받아와서 출력

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
+import kr.co.tjoeun.a20200323_01_loginandsignup.datas.User;
 import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ServerUtil;
 
 public class MainActivity extends BaseActivity {
@@ -40,6 +42,35 @@ public class MainActivity extends BaseActivity {
             public void onResponse(JSONObject json) {
 
                 Log.d("내정보", json.toString());
+
+                try {
+                    int code = 0;
+                    try {
+                        code = json.getInt("code");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (code == 200) {
+                        JSONObject data = json.getJSONObject("data");
+                        JSONObject user = data.getJSONObject("user");
+
+                        final User myInfo = User.getUserFromJson(user);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                binding.nameTxt.setText(myInfo.getName());
+                                binding.memoTxt.setText(myInfo.getMemo());
+                                binding.phoneTxt.setText(myInfo.getPhone());
+                            }
+                        });
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
