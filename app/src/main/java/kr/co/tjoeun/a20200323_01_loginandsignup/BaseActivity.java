2df4866d01c4e435f63ad30cfca2b01a83da5295
potential20 <passion212@nate.com>
2020-03-23 09:42:29 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/BaseActivity.java
@@ -0,0 +1,13 @@
+package kr.co.tjoeun.a20200323_01_loginandsignup;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents() ;
+    public abstract void setValues();
+}
