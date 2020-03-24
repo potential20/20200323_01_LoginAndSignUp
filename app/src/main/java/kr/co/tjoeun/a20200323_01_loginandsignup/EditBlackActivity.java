6f2300e8a1a52738d6c5e11365ee9137b5d844d3
potential20 <passion212@nate.com>
2020-03-24 16:52:11 +0900

    게시글 작성화면 기초작업

---
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/EditBlackActivity.java
@@ -0,0 +1,31 @@
+package kr.co.tjoeun.a20200323_01_loginandsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityEditBlackBinding;
+
+public class EditBlackActivity extends BaseActivity {
+
+    ActivityEditBlackBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_black);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
