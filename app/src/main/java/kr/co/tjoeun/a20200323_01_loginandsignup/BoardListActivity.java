f331092b60b045c3cca314fdd96bb83117c38366
potential20 <passion212@nate.com>
2020-03-24 15:15:36 +0900

    게시판 화면 초안 구현

---
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/BoardListActivity.java
@@ -0,0 +1,32 @@
+package kr.co.tjoeun.a20200323_01_loginandsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityBoardListBinding;
+
+public class BoardListActivity extends BaseActivity {
+
+    ActivityBoardListBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_board_list);
+        setupEvents();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
