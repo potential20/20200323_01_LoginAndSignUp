71a244a33ee733ed81552165cb118f8000233ff0
potential20 <passion212@nate.com>
2020-03-24 14:39:36 +0900

    스플래쉬 액티비티가 시작점이 되도록 변경

---
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/SplashActivity.java
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.a20200323_01_loginandsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
